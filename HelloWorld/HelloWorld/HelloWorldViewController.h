26b650fe6b7c77c3f1b3807f3548c28f251ff8d1
george <george@schoox.com>
2013-05-01 13:29:55 +0300

    The first empty project

---
+++ HelloWorld/HelloWorld/HelloWorldViewController.h
@@ -0,0 +1,13 @@
+//
+//  HelloWorldViewController.h
+//  HelloWorld
+//
+//  Created by George Pilikidis on 5/1/13.
+//  Copyright (c) 2013 schoox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelloWorldViewController : UIViewController
+
+@end
